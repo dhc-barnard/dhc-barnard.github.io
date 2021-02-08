189b6fbc9c3210f47c7f562bc9e3882cc49009db
Ana Lam <alam@barnard.edu>
2021-02-08 13:39:38 -0500

    try hyperlinks

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var pics = ["blob.svg",
-    "blob1.svg",
-    "blob2.svg",
-    "blob3.svg",
-  ];
-
-var url = ["room.html",
-    "room1.html",
-    "room2.html",
-    "room3.html",
-  ];
-
-var ahref = '';
-
-var img = '';
-
-for(ii=0; ii<pics.length; ii++) {
-  positiontop = Math.random()*500;
-  positionright = Math.random()*0;
-  zindex = ii;
-
-  img += '<img src="'+pics[ii]+'" style="width: 25%; top: '+positiontop+'px;'+'right: '+positionright+'px;'+'z-index: '+zindex+';">';
-  ahref += '<a href="'+url[ii]+'"'+img+'</a>'
-}
-
-document.getElementById('area').innerHTML = ahref;
+// var pics = ["blob.svg",
+//     "blob1.svg",
+//     "blob2.svg",
+//     "blob3.svg",
+//   ];
+//
+// var url = ["room.html",
+//     "room1.html",
+//     "room2.html",
+//     "room3.html",
+//   ];
+//
+// var ahref = '';
+//
+// var img = '';
+//
+// for(ii=0; ii<pics.length; ii++) {
+//   positiontop = Math.random()*500;
+//   positionright = Math.random()*0;
+//   zindex = ii;
+//
+//   img += '<img src="'+pics[ii]+'" style="width: 25%; top: '+positiontop+'px;'+'right: '+positionright+'px;'+'z-index: '+zindex+';">';
+//   ahref += '<a href="'+url[ii]+'"'+img+'</a>'
+// }
+//
+// document.getElementById('area').innerHTML = ahref;
