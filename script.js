320f16ff4f1eb5e05b80100134387cd3a3ac3c04
Ana Lam <alam@barnard.edu>
2021-02-08 11:29:04 -0500

    set stylesheet

---
+++ script.js
@@ -0,0 +1,16 @@
+var pics = ["blob.svg",
+    "blob1.svg",
+    "blob2.svg",
+    "blob3.svg",
+  ];
+
+var img = '';
+for(ii=0; ii<pics.length; ii++) {
+  positiontop = Math.random()*500;
+  positionright = Math.random()*0;
+  zindex = ii;
+
+  img += '<img src="'+pics[ii]+'" style="top: '+positiontop+'px;'+'right: '+positionright+'px;'+'z-index: '+zindex+';">';
+}
+
+document.getElementByID('area').innerHTML = img;
