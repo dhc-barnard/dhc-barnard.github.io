00750258a4a1017c0f278741d5f8aae307a9b646
Ana Lam <alam@barnard.edu>
2021-02-08 11:58:43 -0500

    try hyperlinks

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ var pics = ["blob.svg",
     "blob3.svg",
   ];
 
+var url = ["room.html",
+    "room1.html",
+    "room2.html",
+    "room3.html",
+  ];
+
+var ahref = '';
+
 var img = '';
+
 for(ii=0; ii<pics.length; ii++) {
   positiontop = Math.random()*500;
   positionright = Math.random()*0;
   zindex = ii;
 
   img += '<img src="'+pics[ii]+'" style="top: '+positiontop+'px;'+'right: '+positionright+'px;'+'z-index: '+zindex+';">';
+  ahref += '<a href="'+url[ii]+'"'+img+'</a>'
 }
 
-document.getElementById('area').innerHTML = img;
+document.getElementById('area').innerHTML = ahref;
